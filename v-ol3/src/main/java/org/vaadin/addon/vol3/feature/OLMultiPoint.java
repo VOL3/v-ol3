b202cb41508faf507d6177a0bd181f7d731a6653
Chris <chris@limespace.me>
2020-02-02 08:10:09 +0100

    added support for Collection of elements

--- v-ol3/src/main/java/org/vaadin/addon/vol3/feature/OLMultiPoint.java
+++ v-ol3/src/main/java/org/vaadin/addon/vol3/feature/OLMultiPoint.java
@@ -1,11 +1,17 @@
 package org.vaadin.addon.vol3.feature;
 
+import java.util.Collection;
+
 /**
  * Geometry consisting of multiple points
  */
 public class OLMultiPoint extends OLGeometry<OLPoint> {
 
     public OLMultiPoint(OLPoint ... points){
+        super(points);
+    }
+    
+    public OLMultiPoint(Collection<OLPoint> points){
         super(points);
     }
 }
